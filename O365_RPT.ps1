095c92cac14e9ee27573d1a7ddbd01a1cfc03693
Mike <mikhail.sidorenko.ca@gmail.com>
2022-05-19 19:25:51 -0400

    Update

--- O365_RPT.ps1
+++ O365_RPT.ps1
@@ -7,19 +7,34 @@ Connect-SPOService -Url $TenantSiteURL
 $SiteCollections = Get-SPOSite -Limit "3"
 
 $UserData = @()
-
+$updated_items_list = @()
 forEach($Site in $SiteCollections)
 {
     Write-Host "Checking:"$Site.URL
     $Users = Get-SPOUser -Site $Site 
+    
+    forEach($item in $Users){
+        $updated_item_d = $item | Select -ExpandProperty "DisplayName"
+        $updated_item_l = $item | Select -ExpandProperty "LoginName"
+        $updated_item = $updated_item_d + ":" + $updated_item_l
+        $updated_items_list += $updated_item
+
+    }   
+      
     $UserData += New-Object PSObject -Property  @{
         'Site URL' = $Site.URL
         'Users_DisplayName' = ($Users.DisplayName | Out-String).Trim()
         'Users_LoginName' =  ($Users.LoginName | Out-String).Trim()
+        'Users' = ($updated_items_list | Out-String).Trim()
     }
+    
+    Write-Host $updated_item
+    Write-Host $updated_items_list
+
+    Write-Host $Users.count
     Write-Host "Done"
 }
 
-$UserData | Select-Object "Site URL", "Users_DisplayName", "Users_LoginName" | Export-CSV $CSVFilePath -NoTypeInformation
+$UserData | Select-Object "Site URL", "Users_DisplayName", "Users_LoginName", "Users" | Export-CSV $CSVFilePath -NoTypeInformation
 
 Write-Host "Done"
